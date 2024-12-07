184ad37105e597287ab49dfeb36eed66f324fed9
minong00 <s24039@gsm.hs.kr>
2024-12-07 11:48:08 +0900

    람다 함수

---
+++ school/Lambda_2.py
@@ -0,0 +1,7 @@
+list_input_a=[1,2,3,4,5]
+power=lambda x:x*x
+output_a = map(power,list_input_a)
+
+print(map(power, list_input_a),output_a)
+print(map(power, list_input_a),list(output_a))
+
---
+++ school/Lamda.py
@@ -0,0 +1,2 @@
+#1회용 함수 만들때 주로 사용
+power=lambda x: x*x
