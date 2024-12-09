0476911af455f07ca9edbd5861baaf46bb5f8bd7
minong00 <s24039@gsm.hs.kr>
2024-12-09 16:41:54 +0900

    리스트

--- hakone/1_026.py
+++ hakone/1_026.py
@@ -1,12 +1,15 @@
+#a=[]
+#for i in range(0,3):
+#    a.append(input("입력? "))
+#for i in range(0,3):
+#    for j in range(len(a[i])-1,-1,-1):
+#        print(a[i][j],end="")
+#    print()
+
 a=[]
 for i in range(0,3):
     a.append(input("입력? "))
 for i in range(0,3):
-
-    for j in range(0,len(a[i])):
-        temp=a[i][0]
-        a[i][j]=a[i][j+1]
-
-        a[i][len(a[i])]=temp
-
-print(a)
+    for j in range(len(a[i])-3,len(a[i])):
+        print(a[i][j],end="")
+    print()
